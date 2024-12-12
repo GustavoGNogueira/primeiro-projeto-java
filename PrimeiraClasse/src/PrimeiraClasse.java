efe36d78e53ef921df4d3972830a1a1c2afa166f
Gustavo Gomes Nogueira <gu.gomes.nog06@gmail.com>
2024-12-12 17:12:31 -0300

    Add files via upload

---
+++ PrimeiraClasse/src/PrimeiraClasse.java
@@ -0,0 +1,7 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em java!");
+        System.out.println("Nome: Gustavo Gomes Nogueira");
+        System.out.println("Idade: 18 anos");
+    }
+}
