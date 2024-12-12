93855ba4aa735f5dcfb42648298364cf02126259
Gustavo Gomes Nogueira <gu.gomes.nog06@gmail.com>
2024-12-12 17:12:11 -0300

    Add files via upload

---
+++ PrimeiraAplicacao/src/soma.java
@@ -0,0 +1,8 @@
+public class soma {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 5;
+        int resultado = n1 + n2;
+        System.out.println("O resultado da da soma de " + n1 + " e " + n2 + " é " + resultado);
+    }
+}
---
+++ PrimeiraAplicacao/src/subtracao.java
@@ -0,0 +1,9 @@
+public class subtracao {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 5;
+        int resultado = n1 - n2;
+        System.out.println("O resultado da subtração de " + n1 + " e " + n2 + " é " + resultado);
+
+    }
+}
